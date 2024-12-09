6c79dcce61b3cc592b753add7c4d8a0f13dff264
AndrewStattADA <andrew.statt@ada.ac.uk>
2024-12-09 15:06:38 +0000

    2,3, 6 done

--- 03. capitalise/solution.js
+++ 03. capitalise/solution.js
@@ -1,6 +1,6 @@
 function capitalise(word) {
-  word[0] = word[0].toUpperCase();
-  return word;
+  return word.slice(0,1).toUpperCase() + word.slice(1).toLowerCase();
 }
 
+console.log(capitalise("bull"));
 module.exports = { capitalise };
